1ada1ada95ed4fbbdd1498b3d6109240a2324434
destr <destr@DESTROIERRR9MK4>
2018-02-07 23:45:16 -0500

    Added sound

--- Assignment1.1/js/main.js
+++ Assignment1.1/js/main.js
@@ -22,6 +22,10 @@ window.onload = function() {
         game.load.tilemap('map1', 'assets/tiles/tilesmap2_Tile Layer 1.csv', null, Phaser.Tilemap.CSV);
         game.load.tilemap('map2', 'assets/tiles/tilesmap2_Tile Layer 2.csv', null, Phaser.Tilemap.CSV);
         game.load.image('tiles', 'assets/ground.png');
+        game.load.audio('golfhit1', 'assets/sounds/golfhit1.mp3');
+        game.load.audio('golfhit2', 'assets/sounds/golfhit2.mp3');
+        game.load.audio('golfhit3', 'assets/sounds/golfhit3.mp3');
+        game.load.audio('golfhit4', 'assets/sounds/golfhit4.mp3');
     }
     
     let sprite_club;
@@ -36,11 +40,18 @@ window.onload = function() {
     let right;
     let rotateLeft;
     let rotateRight;
+    let golfhit1;
+    let golfhit2;
+    let golfhit3;
+    let golfhit4;
     function create() { //http://phaser.io/examples/v2/tilemaps/csv-map-with-p2
     	game.add.image(0,0,'background');
     	game.physics.startSystem(Phaser.Physics.P2JS);
-    	
-    	map1 = game.add.tilemap('map1', 32, 32);
+    	golfhit1 = game.add.audio('golfhit1');//http://phaser.io/examples/v2/audio/sound-complete
+    	golfhit2 = game.add.audio('golfhit2');
+    	golfhit3 = game.add.audio('golfhit3');
+    	golfhit4 = game.add.audio('golfhit4');
+    	map1 = game.add.tilemap('map1', 32, 32);//https://www.youtube.com/watch?v=8a1uwG-Uefs&t=27s figured out how to use tiled and import tilemaps with this video.
     	map1.addTilesetImage('tiles');
     	map2 = game.add.tilemap('map2', 32, 32);
     	map2.addTilesetImage('tiles');
@@ -62,10 +73,12 @@ window.onload = function() {
     	sprite_ball.scale.setTo(0.5,0.5);
     	game.physics.p2.enable(sprite_club);
     	game.physics.p2.enable(sprite_ball);
-    	
+    	sprite_club.body.createBodyCallback(sprite_ball, hitBall, this); //https://phaser.io/examples/v2/p2-physics/impact-events
+    	game.physics.p2.setImpactEvents(true);
         var help = game.add.text(16, 16, 'WASD TO MOVE | QE TO ROTATE', { font: '14px Arial', fill: '#ffffff' });
         help.fixedToCamera = true;
         
+        //sets controls for wasd and qe //http://www.html5gamedevs.com/topic/7447-use-both-arrow-keys-and-wasd-controls/
     	up = game.input.keyboard.addKey(Phaser.Keyboard.W);
     	down = game.input.keyboard.addKey(Phaser.Keyboard.S);
     	left = game.input.keyboard.addKey(Phaser.Keyboard.A);
@@ -73,7 +86,24 @@ window.onload = function() {
     	rotateLeft = game.input.keyboard.addKey(Phaser.Keyboard.Q);
     	rotateRight = game.input.keyboard.addKey(Phaser.Keyboard.E);
     }
-    
+    function hitBall(body1, body2)
+    {
+    	switch(game.rnd.integerInRange(1,4))//http://phaser.io/examples/v2/misc/random-generators
+    	{
+    	case 1:
+    		golfhit1.play();
+    		break;
+    	case 2:
+    		golfhit2.play();
+    		break;
+    	case 3:
+    		golfhit3.play();
+    		break;
+    	case 4:
+    		golfhit4.play();
+    		break;
+    	}
+    }
     function update() { //https://phaser.io/examples/v2/p2-physics/world-move
     	sprite_club.body.setZeroVelocity();
     	if(left.isDown)
