f251215161f27e0acd1f8f061d418fca4f8085a3
destr <destr@DESTROIERRR9MK4>
2018-02-12 00:58:33 -0500

    forgot to comment out debug :/

--- Assignment2.1/js/main.js
+++ Assignment2.1/js/main.js
@@ -380,7 +380,7 @@ window.onload = function() {
     }
     function render() {
     	//game.debug.text('Active Cleavers: ' + cleavers.countLiving() + ' / ' + cleavers.length, 32, 32);
-        game.debug.text('X:'+ game.input.mousePointer.worldX + ' Y: ' + game.input.mousePointer.worldY,32,32); //MAKES PLACING SPRITES DOWN EASIER OMG
+        //game.debug.text('X:'+ game.input.mousePointer.worldX + ' Y: ' + game.input.mousePointer.worldY,32,32); //MAKES PLACING SPRITES DOWN EASIER OMG
     	//game.debug.cameraInfo(game.camera, 32, 32);
 
     }
